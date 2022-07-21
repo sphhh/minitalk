9ea955e01f4663bafabcbf28fb046f6a20ca20b1
vipereir <zzvtrx@gmail.com>
2022-07-21 17:54:15 -0300

    byte string created

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 10:40:09 by vipereir          #+#    #+#             */
-/*   Updated: 2022/07/21 15:58:57 by vipereir         ###   ########.fr       */
+/*   Updated: 2022/07/21 17:52:36 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,39 @@
 
 void sig_handler(int signum);
 
+char	bit;
+
 int	main(void)
 {
-	int	pid;
+	int		i;
+	int		pid;
+	char	byte[9];
 
+	byte[8] = '\0';
+	i = 0;
 	signal(SIGUSR1, *sig_handler);
 	signal(SIGUSR2, *sig_handler);
 	pid = getpid();
 	ft_printf("pid: %i\n", pid);
 	while (1)
-		pause();
+	{
+		i = 0;
+		while (i < 8)
+		{
+			pause();
+			byte[i] = bit;
+			i++;
+		}
+		ft_printf("%s", byte);
+	}
 	return (0);
 }
 
 void sig_handler(int signum)
 {
 	if (signum == SIGUSR1)
-		write(1, "1", 1);
+		bit = '1';
 	else if (signum == SIGUSR2)
-		write(1, "0", 1);
-	return;
+		bit = '0';
+	return ;
 }
