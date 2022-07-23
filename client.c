13e08a14a6ed0827bfb31830e314bc448baaf465
sphh <zzvtrx@gmail.com>
2022-07-23 13:53:24 -0300

    client segfault protection and warning

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 11:23:36 by vipereir          #+#    #+#             */
-/*   Updated: 2022/07/23 13:35:13 by sphh             ###   ########.fr       */
+/*   Updated: 2022/07/23 13:51:49 by sphh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,9 +38,16 @@ int	main(int argc, char **argv)
 {
 	int		serv_pid;
 
-	if (argc == 0)
+	if (argc == 1)
+	{
+		ft_printf("You forgot the server PID!\n");
 		return(0);
-	(void) argc;
+	}
+	else if (argc == 2)
+	{
+		ft_printf("You forgot the MESSAGE!\n");
+	       	return (0);	
+	}
 	serv_pid = ft_atoi(argv[1]);
 	while (*argv[2])
 		ft_chartobin(serv_pid, *argv[2]++);
