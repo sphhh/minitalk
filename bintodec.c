3ee5f54ccc28f6d17d5683e870e87f46e833b22b
vipereir <zzvtrx@gmail.com>
2022-07-26 16:41:36 -0300

    bin dec

--- bintodec.c
+++ bintodec.c
@@ -6,7 +6,7 @@
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 15:24:35 by vipereir          #+#    #+#             */
-/*   Updated: 2022/07/26 16:00:57 by vipereir         ###   ########.fr       */
+/*   Updated: 2022/07/26 16:41:23 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@ int	main(void)
 {
 	static int	byte;
 	char		*str;
+	int static	i;
 
-	str = "00000011";
+		str = "10001001";
 //	byte = byte | 1;
 //	byte = byte << 1;
 	printf("%i\n", byte);
@@ -27,11 +28,19 @@ int	main(void)
 		{
 			byte = byte << 1;
 			byte = byte | 1;
+			i++;
 		}
-		else
+		else if (*str == '0')
+		{
 			byte = byte << 1;
+			i++;
+		}
 		str++;
+		if (i == 8)
+		{
+			i = 0;
+			printf("%i\n", byte);
+		}
 	}
-	printf("%i\n", byte);
 	return (0);
 }
