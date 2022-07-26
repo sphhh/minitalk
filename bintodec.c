30e45952adbc09c983a0260d99173156a2b5f6cd
vipereir <zzvtrx@gmail.com>
2022-07-26 16:50:20 -0300

    asdf

--- bintodec.c
+++ bintodec.c
@@ -6,7 +6,7 @@
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 15:24:35 by vipereir          #+#    #+#             */
-/*   Updated: 2022/07/26 16:41:23 by vipereir         ###   ########.fr       */
+/*   Updated: 2022/07/26 16:48:35 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main(void)
 	char		*str;
 	int static	i;
 
-		str = "10001001";
+		str = "0000000101000010";
 //	byte = byte | 1;
 //	byte = byte << 1;
 	printf("%i\n", byte);
@@ -40,6 +40,7 @@ int	main(void)
 		{
 			i = 0;
 			printf("%i\n", byte);
+			byte = 0;
 		}
 	}
 	return (0);
